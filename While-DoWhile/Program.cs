69e7aace62c707170900862f8cf196c95794d16e
daniela marques <dd978452@gmail.com>
2023-01-17 10:29:55 -0300

    Adicionar arquivos de projeto.

---
+++ While-DoWhile/Program.cs
@@ -0,0 +1,40 @@
+﻿//estrutura de repetição
+//While: recebe a condiçao e enquanto essa condição for verdadeira ele vai repetir
+using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /*
+                //quando a condição  for pra sempre verdade ela vai ficar em loop infinito
+                       int contador =  0;
+                       while (contador < 20)
+                       {
+                           Console.WriteLine("Rodando o while!");
+                           //contador = contador + 1;
+                           //contador += 1;
+                           contador++; 
+                       }
+                   Console.WriteLine("FIM DA LINHA");
+              */
+
+
+            int contador2 = 0;
+            while (contador2 < 20)
+            {
+                Console.WriteLine("do while!");
+                //contador = contador + 1; outro jeito de fazer
+                //contador += 1;
+                contador2++;
+            }
+            Console.WriteLine("FIM DA LINHA");
+            Console.ReadLine();
+
+        }
+    }
+}
